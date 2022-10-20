3f1b69bb243f069908f9a9f5c9c49b94e049b8ee
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-10-21 00:41:59 +0300

    Added TimerScreenViewModel and TimerScreenContract

--- app/src/main/kotlin/by/ocheretny/chesstimer/MainActivity.kt
+++ app/src/main/kotlin/by/ocheretny/chesstimer/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import by.ocheretny.chesstimer.ui.theme.ChessTimerTheme
+import dagger.hilt.EntryPoint
 
+@EntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,22 +19,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ChessTimerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/kotlin/by/ocheretny/chesstimer/screens/TimerScreen.kt
+++ app/src/main/kotlin/by/ocheretny/chesstimer/screens/TimerScreen.kt
@@ -8,14 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import by.ocheretny.chesstimer.models.Player
 
 private const val TEXT_ROTATION_DEGREES = 180f
